5aab9fa391a0301aac231e9735874f5c382beb96
YU-K <xver456@gmail.com>
2021-05-13 12:57:26 +0400

    Add pages for routing

---
+++ src/components/pages/starships-page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { StarshipDetails, StarshipList } from "../sw-components";
+import Row from "../row";
+
+export default class StarshipsPage extends React.Component {
+  state = {
+    selectedItem: null,
+  };
+
+  onItemSelected = (selectedItem) => {
+    this.setState({ selectedItem });
+  };
+
+  render() {
+    const { selectedItem } = this.state;
+
+    return (
+      <Row
+        left={<StarshipList onItemSelected={this.onItemSelected} />}
+        right={<StarshipDetails itemId={selectedItem} />}
+      />
+    );
+  }
+}
